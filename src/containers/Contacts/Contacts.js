77e16ed4b4ccb93ee7ce2e6d92034c4712d14ec4
galubezzz <galubezzz@gmail.com>
2019-02-25 19:31:13 -0800

    add menu to the contacts page

--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Menu from "../../components/Menu/Menu"
 
 class AboutUs extends Component {
   render() {
     return (
       <div>
+        <Menu/>
         This is our contacts!
       </div>
     );
