d9d509bbb3a81d8fe700200466f15d45ca3efe7c
galubezzz <galubezzz@gmail.com>
2019-02-25 19:28:20 -0800

    added navlinks to menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,7 +5,9 @@ class Menu extends Component {
     render() {
         return (
             <div>
+                <NavLink to="/">Home</NavLink>
                 <NavLink to="/aboutus">About US</NavLink>
+                <NavLink to="/contacts">Contacts</NavLink>
             </div>
         );
     }
