a91852ed7d9673c81ca95a85ba59124fee928729
galubezzz <galubezzz@gmail.com>
2019-02-25 19:02:51 -0800

    add a simple about us container

---
+++ src/containers/AboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        This is home page!
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
