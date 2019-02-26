73aa62222070d22bb4de093b9211268a4d4f461a
galubezzz <galubezzz@gmail.com>
2019-02-25 19:10:08 -0800

    add routing to the App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Home from "./containers/Home/Home";
+import AboutUs from "./containers/AboutUs/AboutUs"
+import Contacts from "./containers/Contacts/Contacts"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/aboutus" component={AboutUs}/>
+                    <Route path="/contacts" component={Contacts}/>
+                    <Route path="/" exact component={Home}/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
