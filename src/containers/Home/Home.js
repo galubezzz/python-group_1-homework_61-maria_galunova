4d59cf8b12a4bec77536b53d8c45175865b17389
galubezzz <galubezzz@gmail.com>
2019-02-25 19:28:41 -0800

    add menu to the home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Menu from "../../components/Menu/Menu"
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Menu/>
         This is home page!
       </div>
     );
