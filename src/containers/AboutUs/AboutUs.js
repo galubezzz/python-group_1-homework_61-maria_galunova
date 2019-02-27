ed3456eaa955be564ee78ab236fafcbafe76394d
galubezzz <galubezzz@gmail.com>
2019-02-27 14:12:54 -0800

    add content to about us page

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import Menu from "../../components/Menu/Menu"
 
 class AboutUs extends Component {
-  render() {
-    return (
-      <div>
-        <Menu/>
-        This is about us!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Menu/>
+                <div className='container'>
+                    <p className='m-5'>Мне нечего рассказать о нас, потому что нет никаких нас. Каждый человек рождается и умирает один.
+                        Никто и никогда не сможет по-настоящему быть рядом с тобой. Все отношения - это лишь иллюзия
+                        близости и нелепая попытка заполнить пустоту внутри.</p>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default AboutUs;
