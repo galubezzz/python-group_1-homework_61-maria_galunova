07e69a7aeb1c7e79828179a6f5b1c5ec351626af
galubezzz <galubezzz@gmail.com>
2019-02-27 14:11:54 -0800

    layout changes in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 class Menu extends Component {
     render() {
         return (
-            <nav className="nav justify-content-end">
+            <nav className="nav justify-content-center">
                 <NavLink className="nav-link" to="/">Home</NavLink>
                 <NavLink className="nav-link" to="/aboutus">About Us</NavLink>
                 <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
