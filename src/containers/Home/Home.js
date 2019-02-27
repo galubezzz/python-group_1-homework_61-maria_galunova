7553981b70112e19d4d321a1d464e2caed71581f
galubezzz <galubezzz@gmail.com>
2019-02-27 14:12:37 -0800

    add content to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import Menu from "../../components/Menu/Menu"
 
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Menu/>
-        This is home page!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Menu/>
+                <div className='container'>
+                    <p className='m-5'>Данная страница не имеет никакого смысла. Впрочем, как и вся наша жизнь. Мы лишь песчинки в
+                        масштабах вселенной, с невероятной скоростью летящие навстречу смерти, которая будет
+                        единственным возможным избавлением.</p>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Home;
